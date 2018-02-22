1ec6ffb7d8836a8bb1e20463aa940739b0191b38
IvanovIvanSergeevich <ya.ivanov.i.s@gmail.com>
2018-02-22 13:24:54 +0300

    Found all love-triangles

--- src/index.js
+++ src/index.js
@@ -3,5 +3,24 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    var i, count=0, frstSpchnee, scndSpchnee, lstSpchnee;
+    var checks = preferences;
+
+    for (i=0; i<preferences.length; i++){
+        frstSpchnee = i+1;
+        scndSpchnee = preferences[i];
+        lstSpchnee = preferences[scndSpchnee-1]
+
+        if (checks[frstSpchnee-1]!=undefined || checks[scndSpchnee-1]!=undefined || checks[lstSpchnee-1]!=undefined) {
+
+            if(preferences[lstSpchnee-1]===frstSpchnee && frstSpchnee!=scndSpchnee) {
+                count++;
+                checks[frstSpchnee - 1] = undefined;
+                checks[scndSpchnee - 1] = undefined;
+                checks[lstSpchnee - 1] = undefined;
+            }
+        }
+    }
+    return count;
 };
+
